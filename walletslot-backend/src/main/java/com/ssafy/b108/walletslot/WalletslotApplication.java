fbc6ee7015a0c2e69e657a184c8df1b2127f21a7
KimDoHun <luckyboyhoon@naver.com>
2025-09-08 21:21:07 +0900

    [chore] 백앤드 프로젝트 생성

---
+++ walletslot-backend/src/main/java/com/ssafy/b108/walletslot/WalletslotApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.b108.walletslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletslotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletslotApplication.class, args);
+    }
+
+}
---
+++ walletslot-backend/src/test/java/com/ssafy/b108/walletslot/WalletslotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.b108.walletslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletslotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
