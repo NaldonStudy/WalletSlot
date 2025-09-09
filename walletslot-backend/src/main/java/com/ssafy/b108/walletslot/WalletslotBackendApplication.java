a7c444bfc77d6eb53538de5ead0d8002e407d909
전해지 <wjsgowl0224@naver.com>
2025-09-09 14:57:24 +0900

    chore: application.yml에 DB 관련 설정 추가

--- walletslot-backend/src/main/java/com/ssafy/b108/walletslot/WalletslotBackendApplication.java
+++ walletslot-backend/src/main/java/com/ssafy/b108/walletslot/WalletslotBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WalletslotApplication {
+public class WalletslotBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WalletslotApplication.class, args);
+        SpringApplication.run(WalletslotBackendApplication.class, args);
     }
 
 }
