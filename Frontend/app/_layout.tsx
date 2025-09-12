6113abf106a79631baaf1bfb6147b6edca3e21a0
joyeun00 <joyeun00@naver.com>
2025-09-12 11:08:38 +0900

    feat: footer 수정 - 아이콘 추가, 색 변경

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout() {
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          {/* 공통 컴포넌트 테스트
+          <Stack.Screen name="(dev)" options={{ headerShown: false }} /> */}
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
