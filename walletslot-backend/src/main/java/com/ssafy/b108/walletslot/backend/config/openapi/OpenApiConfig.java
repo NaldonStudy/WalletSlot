d3135aef81c70caad6d8919aeb40cfc7d0d85945
전해지 <wjsgowl0224@naver.com>
2025-09-14 21:23:09 +0900

    chore: gitlab runner test

--- walletslot-backend/src/main/java/com/ssafy/b108/walletslot/backend/config/openapi/OpenApiConfig.java
+++ walletslot-backend/src/main/java/com/ssafy/b108/walletslot/backend/config/openapi/OpenApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "WalletSlot API 명세서",
+                title = "WalletSlot API 명세서 테스트",
                 description = "SSAFY 13기 특화 프로젝트 WalletSlot API 명세서입니다.",
                 version = "v1.0.0"
         )
