97dd41026a96cabb1a919df66122f2b5bb4001f3
전해지 <wjsgowl0224@naver.com>
2025-09-14 23:51:03 +0900

    chore: CI/CD 테스트

---
+++ walletslot-backend/src/main/java/com/ssafy/b108/walletslot/backend/domain/account/AccountRestController.java
@@ -0,0 +1,17 @@
+package com.ssafy.b108.walletslot.backend.domain.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountRestController {
+
+    // Method
+    @GetMapping
+    public void test() {
+
+    }
+
+}
