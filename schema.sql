a5d0d750647a6323489a20ed98fbcf20d17cb09e
전해지 <wjsgowl0224@naver.com>
2025-09-23 12:12:24 +0900

    fix: transaction 테이블 unique_no 컬럼 속성 수정

--- schema.sql
+++ schema.sql
@@ -179,7 +179,7 @@ CREATE TABLE `transaction` (
   `uuid` VARCHAR(64) NOT NULL,
   `account_id` INT UNSIGNED NOT NULL,
   `account_slot_id` INT UNSIGNED NOT NULL,
-  `unique_no` BIGINT UNSIGNED NOT NULL COMMENT 'transactionUniqueNo',
+  `unique_no` VARCHAR(255) NOT NULL COMMENT 'transactionUniqueNo',
   `type` VARCHAR(64) NOT NULL COMMENT 'transactionType, transactionTypeName',
   `opponent_account_no` BIGINT UNSIGNED COMMENT 'transactionAccountNo',
   `summary` VARCHAR(255) NOT NULL COMMENT 'transactionSummary',
